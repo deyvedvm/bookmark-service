58022ed6e2c18a1e82349a5ae531005c1cadbaa4
Deyve Machado <deyvedvm@hotmail.com>
2022-05-23 17:55:50 -0300

    BookmarkServiceApplication update

--- src/main/java/dev/deyve/bookmarkservice/BookmarkServiceApplication.java
+++ src/main/java/dev/deyve/bookmarkservice/BookmarkServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.util.Objects;
+
 @SpringBootApplication
 public class BookmarkServiceApplication {
 
@@ -15,11 +18,61 @@ public class BookmarkServiceApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(BookmarkServiceApplication.class, args);
 
-        ConfigurableEnvironment configurableEnvironment = configurableApplicationContext.getEnvironment();
+        ConfigurableEnvironment environment = configurableApplicationContext.getEnvironment();
 
-        logger.info("Running Spring Bookmark Service Application!");
-        logger.info("Running on java: {}", System.getProperty("java.version"));
-        logger.info("Profile: {}", configurableEnvironment.getDefaultProfiles());
+        var serverPort = environment.getProperty("server.port");
+        var protocol = getProtocol(environment);
+        var hostAddress = getHostAddress();
+        var contextPath = getContextPath(environment);
+
+        logger.info("Running Spring {} Application!", environment.getProperty("spring.application.name"));
+        logger.info("Running on java: {}", System.getProperty("java.version")); // environment.getProperty("java.version"))
+        logger.info("TimeZone: {}", environment.getProperty("user.timezone"));
+        logger.info("Profile: {}", (Object) getProfile(environment));
+        logger.info("Local: {}", new StringBuilder()
+                .append(protocol)
+                .append("://localhost:")
+                .append(serverPort)
+                .append(contextPath));
+        logger.info("External: {}", new StringBuilder()
+                .append(protocol)
+                .append("://")
+                .append(hostAddress)
+                .append(":")
+                .append(serverPort)
+                .append(contextPath));
     }
 
+    private static String[] getProfile(ConfigurableEnvironment environment) {
+        if (environment.getActiveProfiles().length != 0) {
+            return environment.getActiveProfiles();
+        } else {
+            return environment.getDefaultProfiles();
+        }
+    }
+
+    private static String getProtocol(ConfigurableEnvironment environment) {
+        if (environment.getProperty("server.ssl.key-store") != null) {
+            return "https";
+        } else {
+            return "http";
+        }
+    }
+
+    private static String getHostAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            logger.error("Get Host Address Error: {}", e.getMessage());
+            return "localhost";
+        }
+    }
+
+    private static String getContextPath(ConfigurableEnvironment environment) {
+        if (environment.getProperty("server.servlet.context-path") == null || Objects.requireNonNull(environment.getProperty("server.servlet.context-path")).isEmpty()) {
+            return "/";
+        } else {
+            return environment.getProperty("server.servlet.context-path");
+        }
+    }
 }
